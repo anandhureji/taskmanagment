cc400ceaba95e38d69fd27380f84645cf7203af7
anandhureji <anandhureji33@gmail.com>
2025-03-26 21:45:02 +0530

    added submission service

---
+++ submissionservice/src/test/java/com/tm/submissionservice/SubmissionserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tm.submissionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubmissionserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
